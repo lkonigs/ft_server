5d22c217f6e067dff892fe670adf4e19c29cde66
lkonig <lkonig@student.42.fr>
2021-01-12 12:12:44 +0100

    upd(php): updated php version

--- srcs/start.sh
+++ srcs/start.sh
@@ -32,16 +32,19 @@ echo "FLUSH PRIVILEGES;" | mysql -u root
 # tar xvf /var/www/localhost/phpMyAdmin-4.9.7-english.tar.gz
 # mv phpMyAdmin-4.9.7-english var/www/localhost/phpmyadmin
 # mv phpMyAdmin-4.9.7-english phpmyadmin
-wget https://files.phpmyadmin.net/phpMyAdmin/4.9.0.1/phpMyAdmin-4.9.0.1-all-languages.tar.gz
-tar xvf phpMyAdmin-4.9.0.1-all-languages.tar.gz
-mv phpMyAdmin-4.9.0.1-all-languages var/www/localhost/phpmyadmin
+wget https://files.phpmyadmin.net/phpMyAdmin/5.0.4/phpMyAdmin-5.0.4-all-languages.tar.gz
+tar xvf phpMyAdmin-5.0.4-all-languages.tar.gz
+mv phpMyAdmin-5.0.4-all-languages var/www/localhost/phpmyadmin
+# wget https://files.phpmyadmin.net/phpMyAdmin/4.9.0.1/phpMyAdmin-4.9.0.1-all-languages.tar.gz
+# tar xvf phpMyAdmin-4.9.0.1-all-languages.tar.gz
+# mv phpMyAdmin-4.9.0.1-all-languages var/www/localhost/phpmyadmin
 mv ./config.inc.php var/www/localhost/phpmyadmin
 chmod 660 /var/www/localhost/phpmyadmin/config.inc.php
 chown -R www-data:www-data /var/www/localhost/phpmyadmin
 # rm phpMyAdmin-4.9.7-english.tar.gz
 # mv /tmp/phpmyadmin.inc.php /var/www/localhost/phpmyadmin/config.inc.php
 service php7.3-fpm start
-echo "GRANT ALL ON *.* TO 'lkonig'@'localhost' IDENTIFIED BY '123'" | mysql -u root
+echo "GRANT ALL ON *.* TO 'lkonig'@'localhost' IDENTIFIED BY 'password'" | mysql -u root
 echo "FLUSH PRIVILEGES;" | mysql -u root
 
 # Install and init wordpress
