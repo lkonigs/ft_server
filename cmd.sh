4bcb0aaa4024c58cb7a78af59b280efe0f3a2025
lkonig <lkonig@student.42.fr>
2021-02-12 17:21:46 +0100

    removed comments

--- cmd.sh
+++ cmd.sh
@@ -5,7 +5,8 @@ sudo service nginx stop
 sudo service mysql stop
 # service php7.3-fpm stop
 sudo docker build -t server .
-sudo docker run -e AUTOINDEX=off -d -p 80:80 -p 443:443 --name=ft_server server
+sudo docker run -d -p 80:80 -p 443:443 --name=ft_server server
+# sudo docker run -e AUTOINDEX=off -d -p 80:80 -p 443:443 --name=ft_server server
 # sudo docker run --env AUTOINDEX=off -d -p 80:80 -p 443:443 --name=ft_server server
 nmap localhost
 curl localhost:80
